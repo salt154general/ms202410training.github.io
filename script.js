3ebc0977560a6db7554b15d91fefc83279f3a898
salt154general <salt154general@gmail.com>
2024-10-06 17:20:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// GA4初期化
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-// GA4の計測IDを指定
-gtag('config', 'YOUR_GA4_MEASUREMENT_ID', {
-    'send_page_view': false  // 自動でのページビュー送信をオフにする
-});
-
-let isLoggedIn = false;  // ログイン状態のフラグ
-let userName = null;
-
-// ページが読み込まれた際にpage_viewイベントを送信
-function sendPageView() {
-    gtag('event', 'page_view', {
-        'member_status': isLoggedIn ? '会員' : '非会員'
-    });
-}
-
 // ログイン状態を確認して維持
 function checkLoginStatus() {
     const storedUserName = localStorage.getItem('userName');
